6e4aa04593d694cebd0b773a49f05620a58bb68b
juice500ml <juice500ml@gmail.com>
2016-11-20 23:18:48 +0900

    parsecast.py cleanup

--- main/parsecast.py
+++ main/parsecast.py
@@ -1,6 +1,4 @@
 from bs4 import BeautifulSoup as bs
-from multiprocessing import Pool
-from html2text import HTML2Text
 from textrankr import TextRank
 
 import requests
@@ -26,29 +24,20 @@ def parse_link(url):
     soup = bs(req.text, 'html.parser')
 
     title = str(soup.find('h3', {'class': 'ending_tit_new'}).find('img')['alt'])
+
     img_url = soup.find('span', {'class': 'img_wrap2'})
     if img_url:
         img_url = str(img_url.find('img')['src'])
 
     soup = soup.find('div', {'class': 'na_doc'})
-    [tag.extract() for tag in soup.find_all('p', {'class': 'cap'})]
     [tag.extract() for tag in soup.find_all('caption')]
-    dl = soup.find('dl', {'class': 'na_reference'})
-    if not dl:
-        dl = soup.find('div', {'id': 'na_author_top'})
-    if not dl:
-        [tag.extract() for tag in [tag for tag in dl.next_elements]]
-    text_all = str(soup)
-    
-    helper = HTML2Text()
-    helper.ignore_links = True
-    helper.ignore_images = True
-    helper.ignore_tables = True
-    text_all = helper.handle(text_all)
-    
-    for ch in ['#', '/', '*', '_', '>', '&gt', '&lt', ';', ':', '\\']:
-        text_all = text_all.replace(ch, ' ')
-    text_all = ' '.join(text_all.split())
+    [tag.extract() for tag in soup.find_all('p', {'class': 'cap'})]
+    [tag.extract() for tag in soup.find_all('div', {'class': 'tmp_source2'})]
+    [tag.extract() for tag in soup.find_all('div', {'id': 'na_author_top'})]
+    [tag.extract() for tag in soup.find_all('div', {'class': 't_pdate'})]
+    [tag.extract() for tag in soup.find_all('div', {'class': 'na_cmt_bx'})]
+    [tag.extract() for tag in soup.find_all('', {'style': 'display:none'})]
+    text_all = ' '.join(soup.get_text().split())
 
     ret = list()
     ret.append(title)
@@ -64,6 +53,6 @@ def parse_link(url):
     
 
 if __name__ == '__main__':
-    print(parse_link(get_list()[0]))
-    #pool = Pool(16)
-    #ret = pool.map(parse_all, get_list())
+    url_list = get_list()
+    for url in url_list:
+        print(parse_link(url))
