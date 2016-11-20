a9995223ae58bb00216fcecfd90da4955e2b37cc
punkyoon <cryptography@konkuk.ac.kr>
2016-11-20 22:57:42 +0900

    start project / start app / make model

---
+++ main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class ParsedData(models.Model):
+    url = models.TextField(primary_key=True,)
+    title = models.TextField(default = '')
+    image_url = models.TextField(default = '/static/default.png')
+    line1 = models.TextField(default = '')
+    line2 = models.TextField(default = '')
+    line3 = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.url)
